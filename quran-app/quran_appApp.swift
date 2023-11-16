259633047ce313dab4f683ef4a0e3d3679957a83
Magistra <magistraagis@gmail.com>
2023-11-16 13:13:11 +0800

    Initial Commit

---
+++ quran-app/quran_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  quran_appApp.swift
+//  quran-app
+//
+//  Created by magistra aptam on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct quran_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
